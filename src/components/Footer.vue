337c50a5698ea1384698b642bba1d0a20bea696a
Dima <timofeenkodima@gmail.com>
2022-06-12 16:02:23 -0400

    changed footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="footer">
-    <div class="right">Reach me at <a href="mailto:timofeenkodima@gmail.com">TimofeenkoDima@gmail.com</a> or <router-link to="/contact">through here</router-link></div>
+    <div class="right">
+        <div style="float: right;">
+          <a href="mailto:timofeenkodima@gmail.com" target="_blank">
+              <i class="fa fa-envelope-o fa-2x fa-fw"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/dima-tymofiienko-9aa116184/" target="_blank">
+            <i class="fa fa-linkedin fa-2x fa-fw" style="vertical-align: -5%;"></i>
+          </a>
+          <a href="https://github.com/dimaTidev" target="_blank">
+            <i class="fa fa-github fa-2x fa-fw"></i>
+          </a>
+          <a href="https://www.instagram.com/dimatidev/" target="_blank">
+            <i class="fa fa-instagram fa-2x fa-fw"></i>
+          </a>
+          <a href="https://www.facebook.com/profile.php?id=100011299205944" target="_blank">
+            <i class="fa fa-facebook fa-2x fa-fw"></i>
+          </a>
+        </div>
+    </div>
   </div>
 </template>
 
